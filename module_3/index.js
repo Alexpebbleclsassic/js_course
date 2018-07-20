f157d8cb88cde0f732ba7f9ca3588c7f064c054f
Alexpebbleclsassic <normal_extravert@ukr.net>
2018-07-20 19:31:44 +0300

    Lesson_29

--- module_3/index.js
+++ module_3/index.js
@@ -1,18 +1,5 @@
-function test(obj) {
-var inp1 = document.getElementById('CardNumber1');
 
+console.log():
 
-number.value = inp1.value; 
-
-    if (obj.value.length == 19) {
-        var next = obj.nextSibling;
-        while(next.nodeType != 1 && next.nextSibling)
-            next = next.nextSibling;
-        if (next.nodeType == 1)
-            next.focus();
-    }
-}
-
-function validate(inp) {
-inp.value = inp.value.replace(/[^0-9]/, "");
-}
+fsfsfds
+this.console
--- module_3/ui_controller.js
+++ module_3/ui_controller.js
@@ -1,8 +1,31 @@
 import MaskInput from 'mask-input';
- 
-const maskInput = new MaskInput(document.querySelector('.js-input-selector'), {
+
+
+export default class UIController {
+  constructor() {
+    this.cardInput = document.getElementById('card');
+  }
+}
+
+this.initCardInput();
+this.initNameInput();
+
+
+}
+
+const maskInput = new MaskInput(tis.cardInput, {
   mask: '0000-0000-0000-0000',
   alwaysShowMask: true,
   maskChar: '_',
-});
+  onChange: () => {
+    const result = /^\d{4}-\d{4}-\d{4}-\d{4}$/.test(this.nameInput.value);
+    let color;
 
+    is (result) {
+      color = 'green';
+    } else {
+      color = 'red';
+    }
+    this.nameInput.style
+  }
+});
